4149b3825ef2fb7f2ae747e07be19fda151d83f4
AnnihilatorSama <annihilatorjculver@gmail.com>
2021-12-02 18:26:27 +0000

    No touchie

--- basicscrape.py
+++ basicscrape.py
@@ -1,1 +1,18 @@
 #Ver 1 of the script
+import lxml.html
+import requests
+
+def main():
+	userInput = raw_input("Enter the website you want to scrape (ex Amazon.com): ")
+	userInput='https://www.'+userInput
+	print(userInput)
+	
+	product = raw_input("What product would you like to search for: ")
+	print(product)
+	
+	page = requests.get(userInput)
+	tree = lxml.html.fromstring(page.content)
+	print(tree)
+
+if __name__ == "__main__":
+	main()
