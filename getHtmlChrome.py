10cf7500160e34f1f4739ff13b7a92a018ea9e8a
blue <codyjacobsen59@gmail.com>
2022-03-22 14:50:58 -0400

    #

--- getHtmlChrome.py
+++ getHtmlChrome.py
@@ -1,5 +1,7 @@
+import json
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 #do path to executable path in wedrive.chrome
 
 def main():
@@ -8,11 +10,17 @@ def main():
     options.headless = True
     driver = webdriver.Chrome(options=options)
 
-    driver.get("https://www.amazon.com")
+    driver.get("view-source:https://www.amazon.com")
 
-    f = open(".\page_source\chrome", "wb")
-    f.write((driver.page_source).encode('ascii','ignore'))
-    f.close()
+    #test
+    pre = driver.page_source
+    pre = driver.find_elements(by = By.TAG_NAME, value="pre").text
+    data = json.loads(pre)
+    print(data)
+
+    #f = open(".\page_source\chrome", "wb")
+    #f.write((driver.page_source).encode('ascii','ignore'))
+    #f.close()
 
     driver.quit()
 
