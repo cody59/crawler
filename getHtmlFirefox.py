92124de4d8e7509347b258b90c3edabab6b19b49
Culver <annihilatorjculver@gmail.com>
2022-03-22 12:56:35 -0400

    firefox html

--- getHtmlFirefox.py
+++ getHtmlFirefox.py
@@ -8,7 +8,12 @@ def main():
     driver = webdriver.Firefox(options=options)
     # put execution path into parenthesis to make code easily distributable
     driver.get("https://www.newegg.com/p/pl?d=gpu")
-    gpus = driver.find_element(by=By.XPATH, value='')
+    #gpus = driver.find_element(by=By.XPATH, value='')
+
+    source = open(r"source.txt", "wb")
+    source.write((driver.page_source).encode('ascii', 'ignore'))
+    source.close()
+
     print(driver.page_source)
     driver.quit()
 
