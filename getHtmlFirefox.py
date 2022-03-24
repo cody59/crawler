9c2fb1018f407e0fad9228629f80f884836ab0cf
Culver <annihilatorjculver@gmail.com>
2022-03-24 09:24:44 -0400

    firefox html

--- getHtmlFirefox.py
+++ getHtmlFirefox.py
@@ -1,14 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+import os
 
 def main():
 
     options = Options()
     options.headless = True
-    driver = webdriver.Firefox(options=options)
+    driver = webdriver.Firefox(options=options, service_log_path='geckodriver.log')
     # put execution path into parenthesis to make code easily distributable
     driver.get("https://www.newegg.com/p/pl?d=gpu")
-    #gpus = driver.find_element(by=By.XPATH, value='')
+    # gpus = driver.find_element(by=By.XPATH, value='')
 
     source = open(r"source.txt", "wb")
     source.write((driver.page_source).encode('ascii', 'ignore'))
