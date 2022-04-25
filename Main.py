1fdb1f7c9d77db566cbc8c87e5357715b18ae17e
blue <codyjacobsen59@gmail.com>
2022-04-25 14:43:09 -0400

    update

--- Main.py
+++ Main.py
@@ -138,11 +138,24 @@ def getHtmlOpera():
 
 def amazon(driver, file, keyword):
 
-    for page in range(1, 31): #in the future look for need help for amazon so you can stop at a specific page
+    for page in range(1, 8): #in the future look for need help for amazon so you can stop at a specific page
 
         driver.get(f"https://www.amazon.com/s?k={keyword}&page={page}")
         file.write(driver.page_source.encode('ascii', 'ignore'))
 
+
+def newegg(driver, file, keyword):
+
+    for page in range(1, 8):
+
+        driver.get(f"https://www.newegg.com/p/pl?d={keyword}&page={page}")
+        file.write(driver.page_source.encode('ascii', 'ignore'))
+
+def microcenter(driver, file, keyword):
+
+    for page in range(1, 8):
+        driver.get(f"https://www.microcenter.com/search/search_results.aspx?NTX=mode+MatchPartial&NTT={keyword}&NTK=all&page={page}")
+        file.write(driver.page_source.encode('ascii', 'ignore'))
 
 if __name__ == "__main__":
 
