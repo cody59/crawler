6ae00554117a763711bfd2ebcdf128cbaecfab00
blue <codyjacobsen59@gmail.com>
2022-05-18 17:31:11 -0400

    added scrapy

--- websites.py
+++ websites.py
@@ -1,4 +1,5 @@
 import scrapy
+from multiprocessing import Pool, cpu_count
 
 def choose(driver, file):
 
@@ -7,29 +8,47 @@ def choose(driver, file):
 
     if category.lower() == "tech":
 
-        tech(driver, file, keyword)
+        urls = tech(driver, file, keyword)
 
+        #p = Pool(cpu_count()-1)
+        #p.map(scrape, urls)
+        #p.terminate()
+        #p.join()
+
+def scrape(urls):
+    print("C:\\Users\\blue\\.wdm\\drivers\\geckodriver\\win64\\v0.31.0")
+
+    print(urls)
 
 def tech(driver, file, keyword):
 
     page = 1
+    urls = list()
 
-    #amazon
-    while page != 11: #in the future look for need help for amazon so you can stop at a specific page
+    while page != 11:
 
         #amazon
-        driver.get(f"https://www.amazon.com/s?k={keyword}&page={page}")
-        file.write(driver.page_source.encode('ascii', 'ignore'))
+        #driver.get(f"https://www.amazon.com/s?k={keyword}&page={page}")
+        #file.write(driver.page_source.encode('ascii', 'ignore'))
+
+        urls.append(f"https://www.amazon.com/s?k={keyword}&page={page}")
 
         #newegg
-        driver.get(f"https://www.newegg.com/p/pl?d={keyword}&page={page}")
-        file.write(driver.page_source.encode('ascii', 'ignore'))
+        #driver.get(f"https://www.newegg.com/p/pl?d={keyword}&page={page}")
+        #file.write(driver.page_source.encode('ascii', 'ignore'))
+
+        urls.append(f"https://www.newegg.com/p/pl?d={keyword}&page={page}")
 
         #microcenter
-        driver.get(f"https://www.microcenter.com/search/search_results.aspx?NTX=mode+MatchPartial&NTT={keyword}&NTK=all&page={page}")
-        file.write(driver.page_source.encode('ascii', 'ignore'))
+        #driver.get(f"https://www.microcenter.com/search/search_results.aspx?NTX=mode+MatchPartial&NTT={keyword}&NTK=all&page={page}")
+        #file.write(driver.page_source.encode('ascii', 'ignore'))
+
+        urls.append(f"https://www.microcenter.com/search/search_results.aspx?NTX=mode+MatchPartial&NTT={keyword}&NTK=all&page={page}")
 
         page += 1
+
+    return urls
+
 
 
 #homegoods
